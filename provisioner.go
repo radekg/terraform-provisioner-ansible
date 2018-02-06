22207b77765fa3d9d0b9d9bddd146ee9b0bacfcb
radekg <radek@gruchalski.com>
2018-02-07 00:20:38 +0100

    Split provisioner into multiple files.

--- provisioner.go
+++ provisioner.go
@@ -7,7 +7,6 @@ import (
   "crypto/md5"
   "errors"
   "encoding/hex"
-  "encoding/json"
   "fmt"
   "io"
   "io/ioutil"
@@ -44,86 +43,15 @@ const (
   defaultLimit string = ""
   defaultVaultPasswordFile string = ""
   defaultVerbose string = ""
-  defaultVerbose_Set string = "no"
   // playbook only:
   defaultForceHandlers string = ""
-  defaultForceHandlers_Set string = "no"
   defaultStartAtTask string = ""
   // module only:
   defaultBackground int = 0
   defaultHostPattern string = "all"
   defaultOneLine string = ""
-  defaultOneLine_Set string = "no"
   defaultPoll int = 15
 )
-
-const installerProgramTemplate = `#!/usr/bin/env bash
-if [ -z "$(which ansible-playbook)" ]; then
-  
-  # only check the cloud boot finished if the directory exists
-  if [ -d /var/lib/cloud/instance ]; then
-    until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
-      sleep 1
-    done
-  fi
-
-  # install dependencies
-  if [[ -f /etc/redhat-release ]]; then
-    yum update -y \
-    && yum groupinstall -y "Development Tools" \
-    && yum install -y python-devel
-  else
-    apt-get update \
-    && apt-get install -y build-essential python-dev
-  fi
-
-  # install pip, if necessary
-  if [ -z "$(which pip)" ]; then
-    curl https://bootstrap.pypa.io/get-pip.py | sudo python
-  fi
-
-  # install ansible
-  pip install {{ .AnsibleVersion}}
-
-else
-
-  expected_version="{{ .AnsibleVersion}}"
-  installed_version=$(ansible-playbook --version | head -n1 | awk '{print $2}')
-  installed_version="ansible==$installed_version"
-  if [[ "$expected_version" = *"=="* ]]; then
-    if [ "$expected_version" != "$installed_version" ]; then
-      pip install $expected_version
-    fi
-  fi
-  
-fi
-`
-
-const inventoryTemplate_Remote = `{{$top := . -}}
-{{range .Hosts -}}
-{{.}} ansible_connection=local
-{{end}}
-
-{{range .Groups -}}
-[{{.}}]
-{{range $top.Hosts -}}
-{{.}} ansible_connection=local
-{{end}}
-
-{{end}}`
-
-const inventoryTemplate_Local = `{{$top := . -}}
-{{range .Hosts -}}
-{{.}}
-{{end}}
-
-{{range .Groups -}}
-[{{.}}]
-{{range $top.Hosts -}}
-{{.}}
-{{end}}
-
-{{end}}`
 
 var yesNoStates = map[string]bool{"yes": true, "no": true}
 var becomeMethods = map[string]bool{"sudo": true, "su": true, "pbrun": true, "pfexec": true, "doas": true, "dzdo": true, "ksu": true, "runas": true}
@@ -171,149 +99,6 @@ type ansibleCallArgs struct {
   Tags          []string
   // shared:
   Shared        ansibleCallArgsShared
-}
-
-// -- play:
-
-type play struct {
-  InventoryMeta ansibleInventoryMeta
-  Callable      string
-  CallableType  AnsibleCallbaleType
-  CallArgs      ansibleCallArgs
-}
-
-func (p *play) ToCommand(inventoryFile string, vaultPasswordFile string) (string, error) {
-
-  command := ""
-  // entity to call:
-  if p.CallableType == AnsibleCallable_Playbook {
-
-    command = fmt.Sprintf("ansible-playbook %s", p.Callable)
-
-    // force handlers:
-    if p.CallArgs.ForceHandlers == "yes" {
-      command = fmt.Sprintf("%s --force-handlers", command)
-    }
-    // skip tags:
-    if len(p.CallArgs.SkipTags) > 0 {
-      command = fmt.Sprintf("%s --skip-tags='%s'", command, strings.Join(p.CallArgs.SkipTags, ","))
-    }
-    // start at task:
-    if len(p.CallArgs.StartAtTask) > 0 {
-      command = fmt.Sprintf("%s --start-at-task='%s'", command, p.CallArgs.StartAtTask)
-    }
-    // tags:
-    if len(p.CallArgs.Tags) > 0 {
-      command = fmt.Sprintf("%s --tags='%s'", command, strings.Join(p.CallArgs.Tags, ","))
-    }
-
-  } else if p.CallableType == AnsibleCallable_Module {
-
-    hostPattern := p.CallArgs.HostPattern
-    if hostPattern == "" {
-      hostPattern = defaultHostPattern
-    }
-    command = fmt.Sprintf("ansible %s --module-name='%s'", hostPattern, p.Callable)
-    
-    if p.CallArgs.Background > 0 {
-      command = fmt.Sprintf("%s --background=%s", command, p.CallArgs.Background)
-      if p.CallArgs.Poll > 0 {
-        command = fmt.Sprintf("%s --poll=%s", command, p.CallArgs.Poll)
-      }
-    }
-    // module args:
-    if len(p.CallArgs.Args) > 0 {
-      args := make([]string, 0)
-      for mak, mav := range p.CallArgs.Args {
-        args = append(args, fmt.Sprintf("%s=%+v", mak, mav))
-      }
-      command = fmt.Sprintf("%s --args=\"%s\"", command, strings.Join(args, " "))
-    }
-    // one line:
-    if p.CallArgs.OneLine == "yes" {
-      command = fmt.Sprintf("%s --one-line", command)
-    }
-
-  }
-  // inventory file:
-  command = fmt.Sprintf("%s --inventory-file='%s'", command, inventoryFile)
-
-  // shared arguments:
-
-  // become:
-  if p.CallArgs.Shared.Become == "yes" {
-    command = fmt.Sprintf("%s --become", command)
-    if p.CallArgs.Shared.BecomeMethod != "" {
-      command = fmt.Sprintf("%s --become-method='%s'", command, p.CallArgs.Shared.BecomeMethod)
-    } else {
-      command = fmt.Sprintf("%s --become-method='%s'", command, defaultBecomeMethod_Set)
-    }
-    if p.CallArgs.Shared.BecomeUser != "" {
-      command = fmt.Sprintf("%s --become-user='%s'", command, p.CallArgs.Shared.BecomeUser)
-    } else {
-      command = fmt.Sprintf("%s --become-user='%s'", command, defaultBecomeUser_Set)
-    }
-  }
-  // extra vars:
-  if len(p.CallArgs.Shared.ExtraVars) > 0 {
-    extraVars, err := json.Marshal(p.CallArgs.Shared.ExtraVars)
-    if err != nil {
-      return "", err
-    }
-    command = fmt.Sprintf("%s --extra-vars='%s'", command, string(extraVars))
-  }
-  // forks:
-  if p.CallArgs.Shared.Forks > 0 {
-    command = fmt.Sprintf("%s --forks=%d", command, p.CallArgs.Shared.Forks)
-  }
-  // limit
-  if len(p.CallArgs.Shared.Limit) > 0 {
-    command = fmt.Sprintf("%s --limit='%s'", command, p.CallArgs.Shared.Limit)
-  }
-  // vault password file:
-  if len(vaultPasswordFile) > 0 {
-    command = fmt.Sprintf("%s --vault-password-file='%s'", command, vaultPasswordFile)
-  }
-  // verbose:
-  if p.CallArgs.Shared.Verbose == "yes" {
-    command = fmt.Sprintf("%s --verbose", command)
-  }
-
-  return command, nil
-}
-
-// -- runnable play:
-
-type runnablePlay struct {
-  Play                   play
-  VaultPasswordFile      string
-  InventoryFile          string
-  InventoryFileTemporary bool
-}
-
-func (r *runnablePlay) ToCommand() (string, error) {
-  return r.Play.ToCommand(r.InventoryFile, r.VaultPasswordFile)
-}
-
-func (r *runnablePlay) ToLocalCommand(o terraform.UIOutput, rpla runnablePlayLocalAnsibleArgs) (string, error) {
-  baseCommand, err := r.ToCommand()
-  if err != nil {
-    return "", err
-  }
-  return fmt.Sprintf("%s %s", baseCommand, rpla.ToCommandArguments()), nil
-}
-
-type runnablePlayLocalAnsibleArgs struct {
-  Username       string
-  PemFile        string
-  KnownHostsFile string
-}
-
-func (rpla *runnablePlayLocalAnsibleArgs) ToCommandArguments() string {
-  args := fmt.Sprintf("--private-key='%s'", rpla.PemFile)
-  args = fmt.Sprintf("%s --user='%s'", args, rpla.Username)
-  args = fmt.Sprintf("%s --ssh-extra-args='-o UserKnownHostsFile=%s'", args, rpla.KnownHostsFile)
-  return args
 }
 
 // -- provisioner:
