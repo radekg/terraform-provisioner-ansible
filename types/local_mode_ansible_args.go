b04b3354834880c9e88dd9358edd82479436705b
radekg <radek@gruchalski.com>
2018-11-04 21:55:12 +0100

    Local ansible args: add BastionKnownHostsFile.

--- types/local_mode_ansible_args.go
+++ types/local_mode_ansible_args.go
@@ -3,12 +3,13 @@ package types
 // LocalModeAnsibleArgs is used by the local provisioner
 // to feed Ansible with correct connection setup data.
 type LocalModeAnsibleArgs struct {
-	Username        string
-	Port            int
-	PemFile         string
-	KnownHostsFile  string
-	BastionUsername string
-	BastionHost     string
-	BastionPort     int
-	BastionPemFile  string
+	Username              string
+	Port                  int
+	PemFile               string
+	KnownHostsFile        string
+	BastionKnownHostsFile string
+	BastionUsername       string
+	BastionHost           string
+	BastionPort           int
+	BastionPemFile        string
 }
