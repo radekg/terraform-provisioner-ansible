9f97685693e8b1294ed12b1e18c01a309f1d1c1f
radekg <radek@gruchalski.com>
2019-09-30 19:07:48 +0200

    Further fixes for #130 remote installer shell script

--- mode/mode_remote.go
+++ mode/mode_remote.go
@@ -33,7 +33,7 @@ set -eu
 if [ -z "$(which ansible-playbook)" ]; then
   # only check the cloud boot finished if the directory exists
   if [ -d /var/lib/cloud/instance ]; then
-    until [[ -f /var/lib/cloud/instance/boot-finished ]]; do
+    while [ -f /var/lib/cloud/instance/boot-finished ]; do
       sleep 1
     done
   fi
@@ -56,11 +56,13 @@ else
   expected_version="{{ .AnsibleVersion}}"
   installed_version=$(ansible-playbook --version | head -n1 | awk '{print $2}')
   installed_version="ansible==$installed_version"
-  if [[ "$expected_version" = *"=="* ]]; then
-    if [ "$expected_version" != "$installed_version" ]; then
-      pip install $expected_version
-    fi
-  fi
+  case "$expected_version" in
+	*==*)
+	  if [ "$expected_version" != "$installed_version" ]; then
+        pip install $expected_version
+	  fi
+	;;
+  esac
 fi
 `
 
