960e83cb8385eea8812e82283f8ee4274973785a
radekg <radek@gruchalski.com>
2020-11-07 22:34:20 +0100

    Move provisioner to separate package. Closes #134

--- main.go
+++ main.go
@@ -3,12 +3,13 @@ package main
 import (
 	"github.com/hashicorp/terraform/plugin"
 	"github.com/hashicorp/terraform/terraform"
+	"github.com/radekg/terraform-provisioner-ansible/v2/provisioner"
 )
 
 func main() {
 	plugin.Serve(&plugin.ServeOpts{
 		ProvisionerFunc: func() terraform.ResourceProvisioner {
-			return Provisioner()
+			return provisioner.Provisioner()
 		},
 	})
 }
